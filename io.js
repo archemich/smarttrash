db95537f97a7bd5886d4c50817e0896b071177a9
MegaVasiliy007 <vasiliy072000mail.ru@gmail.com>
2020-01-31 22:41:39 +0300

    add manager function and some fix

--- io.js
+++ io.js
@@ -13,9 +13,27 @@ module.exports = function (io, con) {
 			user = jwt.decodeJWT(user).login;
 			
 			let data = await con.promise().query('SELECT idtrash, lat, lng FROM trashs WHERE idtrash IN (SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(users.way, \',\', numbers.n), \',\', -1) name FROM (SELECT 1 n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4) numbers INNER JOIN users ON CHAR_LENGTH(users.way)-CHAR_LENGTH(REPLACE(users.way, \',\', \'\'))>=numbers.n-1 WHERE login = ? ORDER BY n)', [user]);
-			console.log(data[0]);
 			socket.emit('sendDriverPath', data[0].map(el => [el.lat, el.lng]));
 		});
 		
+		socket.on('getManagerData', async (data, filter) => {
+			let sendData = {};
+			if (data.trash) {
+				switch (filter) {
+					case 'percent':filter = 'percent DESC';break;
+					default:filter = 'idtrash ASC';
+				}
+				let trashs = await con.promise().query(`SELECT * FROM trashs ORDER BY ${filter}`);
+				sendData.trashs = trashs[0];
+			}
+			if (data.driver) {
+				let drivers = await con.promise().query('SELECT * FROM users WHERE login != \'manager\'');
+				sendData.drivers = drivers[0];
+			}
+			socket.emit('sendManagerData', sendData);
+		});
+		
+		socket.on('addWay', (login, way) => {con.update('users', {login, way}, () => {}); con.query('UPDATE trashs SET forClean = 1 WHERE idtrash IN (?)', [way.split(',')], () => {})});
+		socket.on('delWay', (login) => {con.queryValue('SELECT way FROM users WHERE login = ?', [login], (err, way) => {con.query('UPDATE trashs SET forClean = 0 WHERE idtrash IN (?)', [way.split(',')], () => {})}); con.update('users', {login, way: null}, () => {})});
 	});
 };
--- main.js
+++ main.js
@@ -12,6 +12,7 @@ con.connect((err) => {if (err) return console.error('error connecting: ' + err.s
 let util = require('mysql-utilities');
 util.upgrade(con);
 util.introspection(con);
+con.query("SET SESSION wait_timeout = 604800");
 
 app.use(express.static('public'));
 
@@ -20,6 +21,11 @@ try {require('./io')(io, con)} catch (e) {console.warn(e)}
 function checkAccess(req, res, next) {
 	if (!req.cookies.login) {res.redirect('/login');return;}
 	if (!jwt.verifyJWT(req.cookies.login)) {res.clearCookie('login');res.redirect('/login');return;}
+	
+	let user = jwt.decodeJWT(req.cookies.login).login;
+	if (['manager'].indexOf(user) == -1 && req.originalUrl == '/manager') {res.redirect('/driver'); return;}
+	else if (['manager'].indexOf(user) != -1 && req.originalUrl == '/driver') {res.redirect('/manager'); return;}
+	
 	next();
 }
 
@@ -53,19 +59,25 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/driver', checkAccess, (req, res) => {
-	if (['manager'].indexOf(req.body.login) != -1) {res.redirect('/manager'); return;}
-	
 	res.sendFile(process.env.PWD+'/views/driver.html');
 });
 
 app.get('/manager', checkAccess, (req, res) => {
-	if (['manager'].indexOf(req.body.login) == -1) {res.redirect('/driver'); return;}
-	
 	res.sendFile(process.env.PWD+'/views/manager.html');
+});
+
+app.post('/trash', (req, res) => {
+	if (!req.body.per || !req.body.id || !req.body.batt) {
+		res.end();
+		return;
+	}
+	console.log(req.body);
+	con.query('UPDATE trashs SET percent = ?, battery = ? WHERE idtrash = ?', [req.body.per, req.body.batt, req.body.id]);
+	res.end();
 });
 
 // Handle 404 AND 500
 app.use((req, res) => res.status(404).send('404 Error <a href="/">Home page</a>'))
 	.use((error, req, res) => {console.warn(error); res.status(500).send('500 Error <a href="/">Home page</a>');});
 
-http.listen(8000, () => console.log('Work on port :8000'));
+http.listen(8001, () => console.log('Work on port :8001'));
